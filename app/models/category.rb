945bfc152c7897a0dafe5d44987cf8c7ede45166
Chris Kheng <chriskheng1609@gmail.com>
2020-01-12 17:48:50 +0800

    Add delete TaskCategory association upon deleting a category feature

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
-    has_many :task_categories
+    has_many :task_categories, dependent: :destroy
     has_many :tasks, through: :task_categories
     validates :name, presence: true, length: {maximum: 60}
     validates_uniqueness_of :name
