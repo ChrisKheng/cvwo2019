646f93c5d2d0f665ef91c823f269a73f759ed5b7
Chris Kheng <chriskheng1609@gmail.com>
2020-01-14 21:52:50 +0800

    Refactor rails routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#home'
   get 'about', to: 'pages#home'
+
   get 'app/tasks', to: 'pages#app'
   get 'app/tasks/tags/:tagId', to: 'pages#app'
-
-  get 'tasks', to: 'tasks#index'
-  post 'tasks', to: 'tasks#create'
-  delete 'tasks/:id', to: 'tasks#destroy'
-  put 'tasks/:id', to: 'tasks#update'
-
+  resources :tasks, except: [:new, :edit, :show]
   resources :categories, except: [:new, :edit, :show]
 end
 
